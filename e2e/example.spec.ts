6d20e879d576cb412da5799a1821cd85ac5cc29d
Rahul Makhija <rahul.makhija@razorpay.com>
2024-07-09 11:55:57 +0530

    add more assertions

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -15,4 +15,6 @@ test('get started link', async ({ page }) => {
 
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await page.getByRole('button', { name: 'Guides' }).click();
+  await expect(page.getByRole('link', { name: 'Best Practices' })).toBeVisible();
 });
