106a82fe42d6cf49d0a71512647325c1737e3834
Rahul Makhija <rahul.makhija@razorpay.com>
2024-08-31 09:57:27 +0530

    add nesting examples

--- e2e/nesting.spec.ts
+++ e2e/nesting.spec.ts
@@ -4,46 +4,27 @@ test.describe("Highest Level", () => {
   let x: number;
   x = 1;
 
-  test.beforeAll(() => {
-    console.log("--BEFORE all--");
-  });
-
-  test.beforeEach(() => {
-    console.log("BEFORE each");
-  });
-  
-  test.afterEach(() => {
-    console.log("AFTER each");
-  });
-
-  test.afterAll(() => {
-    console.log("--AFTER all--");
-  });
-
   test.describe("Nesting One", () => {
-    test.beforeEach(() => {
-      console.log("BEFORE each - One");
-    });
     test("One", async ({ page }) => {
       expect(x).toBe(1);
+      console.log("PASSED");
     });
 
     test("One v2", async ({ page }) => {
       expect(x).toBe(1);
+      console.log("PASSED");
     });
   });
 
   test.describe("Nesting Two", () => {
-    test.beforeEach(() => {
-      console.log("BEFORE each - Two");
-    });
     test("Two", async ({ page }) => {
       expect(x).toBe(1);
+      console.log("PASSED");
     });
 
     test("Two v2", async ({ page }) => {
       expect(x).toBe(1);
+      console.log("PASSED");
     });
   });
-
 });
