faec160ee12f37fb7d482b1075789787c2e5e027
Rahul Makhija <rahul.makhija@razorpay.com>
2024-11-19 13:10:37 +0530

    add visual testing examples

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,7 +21,10 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1: 4,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: 'list',
+  expect: {
+    toHaveScreenshot: { maxDiffPixels: 100 },
+  },
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
